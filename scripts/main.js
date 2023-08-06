0598f50b4e37d964fb2f97250b5467af9bd7c196
theripper93 <tsplab@gmail.com>
2023-08-06 16:22:00 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -9,63 +9,6 @@ Hooks.on("init", () => {
     Dialog.input = inputDialog;
 });
 
-Hooks.on("ready", async () => {
-    //Test
-    const dialogData = await Dialog.input({
-        title: "Test",
-        content: "This is a test input dialog, pick something",
-        inputs: {
-            myText: {
-                type: "text",
-                label: "My Text Input",
-                placeholder: "Enter some text here",
-                default: "Hello World"
-            },
-            myNumber: {
-                type: "number",
-                label: "My Number Input",
-                placeholder: "Enter a number here",
-                default: 42
-            },
-            mySelect: {
-                type: "select",
-                label: "My Select Input",
-                options: {
-                    1: "Option 1",
-                    2: "Option 2",
-                    3: "Option 3"
-                },
-                default: 2
-            },
-            myCheckbox: {
-                type: "checkbox",
-                label: "My Checkbox Input",
-                default: true
-            },
-            myColor: {
-                type: "color",
-                label: "My Color Input",
-                default: "#ff0000"
-            },
-            myFilePicker: {
-                type: "filePicker",
-                filePicker: "imagevideo",
-                label: "My File Picker Input",
-                placeholder: "path/to/file"
-            },
-            myRange: {
-                type: "range",
-                label: "My Range Input",
-                min: 0,
-                max: 100,
-                step: 1,
-                default: 50
-            },
-        }
-    });
-    console.log(dialogData);
-})
-
 async function inputDialog({title, content, inputs, render, defaultYes = true, options = {}} = {}) {
     const inputDialogContent = await getHtml(content, inputs);
     return new Promise((resolve, reject) => {
